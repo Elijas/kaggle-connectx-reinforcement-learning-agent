34d5f3decaaaecec435c73a46cb7e690eeec2534
Elijas Dapšauskas <master.elijas@gmail.com>
2020-01-12 16:30:12 +0200

    Refactor debug.py

--- tools/debug.py
+++ tools/debug.py
@@ -2,13 +2,14 @@ from kaggle_environments import make
 
 from agents import selects_leftmost
 
-AGENT = selects_leftmost.act
+AGENT_1 = selects_leftmost.act
+AGENT_2 = 'negamax'
 
 if __name__ == '__main__':
     env = make('connectx', debug=True)
-    trainer = env.train([None, 'negamax'])
+    trainer = env.train([None, AGENT_2])
     observation = trainer.reset()
     while not env.done:
-        my_action = AGENT(observation, env.configuration)
+        my_action = AGENT_1(observation, env.configuration)
         print("My Action", my_action)
         observation, reward, done, info = trainer.step(my_action)
